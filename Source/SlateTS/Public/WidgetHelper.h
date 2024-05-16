0bd38f6e64f3d24789d8b497db039310fb103405
YouLe <1655753014@qq.com>
2024-05-16 19:38:05 +0800

    更新 WidgetHelper.h

--- Source/SlateTS/Public/WidgetHelper.h
+++ Source/SlateTS/Public/WidgetHelper.h
@@ -62,9 +62,9 @@ namespace DTS
 	public:
 		Property& Name(const FString& Value) { _Name = Value; return *this; }
 		Property& Type(const FString& Value) { _Type = Value; return *this; }
-		Property& bOut(const bool& Value) { _bRef = Value; return *this; }
-		Property& bStatic(const bool& Value) { _bStatic = Value; return *this; }
-		Property& bReadonly(const bool& Value) { _bReadonly = Value; return *this; }
+		Property& Out(const bool& Value) { _bRef = Value; return *this; }
+		Property& Static(const bool& Value) { _bStatic = Value; return *this; }
+		Property& Readonly(const bool& Value) { _bReadonly = Value; return *this; }
 
 		FString GenDTS();
 	};
@@ -84,9 +84,9 @@ namespace DTS
 		public:
 			Slot& Name(const FString& Value) { _Name = Value; return *this; }
 			Slot& Parameters(const TArray<Property>& Value) { _Parameters = Value; return *this; }
-			Slot& ReturnPara(const Property& Value) { _ReturnPara = Value; return *this; }
-			Slot& bStatic(const bool& Value) { _bStatic = Value; return *this; }
-			Slot& bVirtual(const bool& Value) { _bVirtual = Value; return *this; }
+			Slot& Return(const Property& Value) { _ReturnPara = Value; return *this; }
+			Slot& Static(const bool& Value) { _bStatic = Value; return *this; }
+			Slot& Virtual(const bool& Value) { _bVirtual = Value; return *this; }
 		};
 
 		Function& operator[](const Slot& Value)
@@ -135,31 +135,31 @@ void Plan()
 	DTS::Class CC = DTS::Class().Name("ClassA").Super("BaseClass")
 		.Arguments(DTS::WidgetArguments())
 		.Properties(DTS::Array<DTS::Property>()
-			+ DTS::Property().Name("P1").Type("bool").bOut(false).bStatic(true).bReadonly(true)
-			+ DTS::Property().Name("P2").Type("bool").bOut(false).bStatic(true).bReadonly(true)
+			+ DTS::Property().Name("P1").Type("bool").Out(false).Static(true).Readonly(true)
+			+ DTS::Property().Name("P2").Type("bool").Out(false).Static(true).Readonly(true)
 		)
 		.Functions(DTS::Array<DTS::Function>()
 			+ DTS::Function()
 			[
 				DTS::Function::Slot().Name("Func1")
 					.Parameters(DTS::Array<DTS::Property>()
-						+ DTS::Property().Name("P1").Type("bool").bOut(false).bStatic(true).bReadonly(true)
-						+ DTS::Property().Name("P2").Type("bool").bOut(false).bStatic(true).bReadonly(true)
+						+ DTS::Property().Name("P1").Type("bool").Out(false).Static(true).Readonly(true)
+						+ DTS::Property().Name("P2").Type("bool").Out(false).Static(true).Readonly(true)
 					)
-					.ReturnPara(DTS::Property().Type("float"))
-					.bStatic(true)
-					.bVirtual(true)
+					.Return(DTS::Property().Type("float"))
+					.Static(true)
+					.Virtual(true)
 			]
 			+ DTS::Function()
 			[
 				DTS::Function::Slot().Name("Func1")
 					.Parameters(DTS::Array<DTS::Property>()
-						+ DTS::Property().Name("P1").Type("bool").bOut(false).bStatic(true).bReadonly(true)
-						+ DTS::Property().Name("P2").Type("bool").bOut(false).bStatic(true).bReadonly(true)
+						+ DTS::Property().Name("P1").Type("bool").Out(false).Static(true).Readonly(true)
+						+ DTS::Property().Name("P2").Type("bool").Out(false).Static(true).Readonly(true)
 					)
-					.ReturnPara(DTS::Property().Type("float"))
-					.bStatic(true)
-					.bVirtual(true)
+					.Return(DTS::Property().Type("float"))
+					.Static(true)
+					.Virtual(true)
 			]
 		);
 
@@ -168,8 +168,8 @@ void Plan()
 	DTS::Class DTS_STextBlock = DTS::Class().Name("STextBlock").Super("SLeafWidget")
 		.Arguments(DTS::WidgetArguments())
 		.Properties(DTS::Array<DTS::Property>()
-			+ DTS::Property().Name("P1").Type("bool").bOut(false).bStatic(true).bReadonly(true)
-			+ DTS::Property().Name("P2").Type("bool").bOut(false).bStatic(true).bReadonly(true)
+			+ DTS::Property().Name("P1").Type("bool").Out(false).Static(true).Readonly(true)
+			+ DTS::Property().Name("P2").Type("bool").Out(false).Static(true).Readonly(true)
 		)
 		.Functions(DTS::Array<DTS::Function>()
 			+ DTS::Function()
@@ -179,29 +179,29 @@ void Plan()
 						+ DTS::Property().Name("Arguments").Type("STextBlock.Arguments")
 						+ DTS::Property().Name("Filename").Type(TS_string)
 					)
-					.ReturnPara(DTS::Property().Type(PUERTS_NAMESPACE::ScriptTypeName<TSharedPtr<STextBlock>>::value().Data()))
-					.bStatic(true)
+					.Return(DTS::Property().Type(PUERTS_NAMESPACE::ScriptTypeName<TSharedPtr<STextBlock>>::value().Data()))
+					.Static(true)
 			]
 			+ DTS::Function()
 			[
 				DTS::Function::Slot().Name("SAssignNew")
 					.Parameters(DTS::Array<DTS::Property>()
-						+ DTS::Property().Name("WidgetRef").Type(PUERTS_NAMESPACE::ScriptTypeName<TSharedPtr<STextBlock>>::value().Data()).bOut(true)
+						+ DTS::Property().Name("WidgetRef").Type(PUERTS_NAMESPACE::ScriptTypeName<TSharedPtr<STextBlock>>::value().Data()).Out(true)
 						+ DTS::Property().Name("Arguments").Type("STextBlock.Arguments")
 						+ DTS::Property().Name("Filename").Type(TS_string)
 					)
-					.bStatic(true)
+					.Static(true)
 			]
 			+ DTS::Function()
 			[
 				DTS::Function::Slot().Name("MakeShared")
-					.ReturnPara(DTS::Property().Type(PUERTS_NAMESPACE::ScriptTypeName<TSharedPtr<STextBlock>>::value().Data()))
-					.bStatic(true)
+					.Return(DTS::Property().Type(PUERTS_NAMESPACE::ScriptTypeName<TSharedPtr<STextBlock>>::value().Data()))
+					.Static(true)
 			]
 			+ DTS::Function()
 			[
 				DTS::Function::Slot().Name("GetText")
-					.ReturnPara(DTS::Property().Type(TS_string))
+					.Return(DTS::Property().Type(TS_string))
 			]
 			+ DTS::Function()
 			[
